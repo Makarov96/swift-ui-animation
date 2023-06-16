ef92816d90e5160811d7fef42a0d3659b5c30869
Makarov96 <estev97@gmail.com>
2023-06-15 21:30:30 -0600

    Initial Commit

---
+++ listanimation/listanimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  listanimationApp.swift
+//  listanimation
+//
+//  Created by Guerin Steven Colocho Chacon on 15/06/23.
+//
+
+import SwiftUI
+
+@main
+struct listanimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
